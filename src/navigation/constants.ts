1f5ef4e7add402a0f4f041d6d7a3d3843ceae6dd
Andrey Sulzhyk <privettandrey@gmail.com>
2022-06-28 21:20:58 +0200

    Fixed navigation

--- src/navigation/constants.ts
+++ src/navigation/constants.ts
@@ -1,6 +1,6 @@
 export const navigation = {
   main: '/',
-  movie: 'movie',
-  search: 'search',
-  favorites: 'favorites',
+  movie: '/movie',
+  search: '/search',
+  favorites: '/favorites',
 };
