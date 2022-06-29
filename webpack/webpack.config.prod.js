4864010fb7607f00ecbcb33fb0fa9d9ad94fd4b4
Andrey Sulzhyk <privettandrey@gmail.com>
2022-06-29 19:28:15 +0200

    Added webpack contenthash and clean

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,9 +1,15 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '..', './dist'),
+    filename: 'app.bundle.[contenthash].js',
+    clean: true,
+  },
   plugins: [
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
